04d59364d8d56b3055e7040fb5c61d2fa4152d90
JW043373 <jwhipple07@gmail.com>
2016-10-25 14:02:14 -0500

    Exercise 10

---
+++ CS349/src/Exercise10/Part1.java
@@ -0,0 +1,33 @@
+package Exercise10;
+
+import java.lang.reflect.Field;
+
+import acme.NetworkService;
+
+/**
+ * Run the unaltered program, to show that it errors.
+ * Then search for the field names and will use that in Part2.
+ * 
+ * @author Jonathan Whipple
+ *
+ */
+public class Part1 {
+	public static void main(String[] args) {
+        runOriginalProgram();
+        System.out.println("");
+		findAttribute();
+    }
+	
+	public static void runOriginalProgram(){
+		NetworkService ns = new NetworkService();
+        ns.connect();
+	}
+	
+	public static void findAttribute(){
+		NetworkService ns = new NetworkService();
+		Field[] fields = ns.getClass().getDeclaredFields();
+		for(Field field : fields){
+			System.out.println("Field Name: " + field.getName() + " | " + field.toGenericString());
+		}
+	}
+}
